ac463e7888563756d8d7ead9547437766333c351
pufferbommy <pufferbommy@gmail.com>
2021-10-28 13:26:52 +0700

    add propTypes

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
+import PropTypes from 'prop-types'
+
 import Content from '../components/Content'
+import Drawer from '../components/Drawer'
 import Footer from '../components/Footer'
 import Navbar from '../components/Navbar'
 
@@ -7,10 +10,16 @@ const Home = ({ nextTheme, setTheme }) => {
     return (
         <div>
             <Navbar nextTheme={nextTheme} setTheme={setTheme} />
+            <Drawer />
             <Content nextTheme={nextTheme} />
             <Footer />
         </div>
     )
+}
+
+Home.propTypes = {
+    nextTheme: PropTypes.func,
+    setTheme: PropTypes.func,
 }
 
 export default Home
